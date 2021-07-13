14438cfb15fa15d0f87e72dec19f807516b93ce0
Henrik Westergård <henrik@westergard.org>
2021-07-13 18:43:49 +0300

    Initial commit

---
+++ src/JuisUtils.js
@@ -0,0 +1,36 @@
+export default {
+    arrayWrap: (values) => {
+        if (!Array.isArray(values)) {
+            if (values === undefined) {
+                values = [];
+            } else {
+                values = [values];
+            }
+        }
+        return values;
+    },
+    camelCaseToDash: text => text.replace(/([A-Z])/g, $1 => "-" + $1.toLowerCase())
+};
+
+function sortByMappedValue(mapper) {
+    return (a, b) => {
+        const mappedA = mapper(a);
+        const mappedB = mapper(b);
+        if (mappedA < mappedB) {
+            return -1;
+        }
+        if (mappedA > mappedB) {
+            return 1;
+        }
+        return 0;
+    }
+}
+
+function removeByValue(array, item) {
+    let index = array.indexOf(item);
+    if (index !== -1) array.splice(index, 1);
+    return array;
+}
+
+export {removeByValue};
+export {sortByMappedValue};
